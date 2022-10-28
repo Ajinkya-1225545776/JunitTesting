8c85f679eadcadb24b6f28bbd12110c7e13ceb3a
Ajinkya Pise <112654786+Ajinkya-1225545776@users.noreply.github.com>
2022-10-27 18:35:40 -0700

    First Commit

---
+++ Test1.java
@@ -0,0 +1,6 @@
+public class Test1 {
+    public Boolean countUrinals(String name){
+        System.out.println("NotCorrect Name");
+        return true;
+    }
+}
---
+++ Urinals.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Urinals {
+    //Author=Ajinkya Abhinay Pise
+
+    @Test
+    void add() {
+        var te =new Test1();
+        assertEquals("Ajinkya" ,te.countUrinals("Ajinkya"));
+    }
+}
+
+
